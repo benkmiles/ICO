6f882c60993cd11ab3086ec497a33f3798c958c6
bennycdr <58797411+bennycdr@users.noreply.github.com>
2022-04-21 16:25:05 -0400

    upload files

--- AnsibleXEDemo/create-inventory.sh
+++ AnsibleXEDemo/create-inventory.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 isappend=0
-while getopts g:s:o:a: flag
+while getopts g:s:o:a flag
 do
     case "${flag}" in
         g) groupname=${OPTARG};;
         s) servers=${OPTARG};;
         o) myoutfile=${OPTARG};;
-        a) isappend=1
+        a) isappend=1;;
     esac
 done
-echo "Append = $isappend"
-if [ -z "$myoutfile" ] 
+if [ -z "$myoutfile" ]
 then
-	myoutfile=custom_inventory
+        myoutfile=custom_inventory
 fi
 
-if ("$isappend" == "1") then
-    echo "$groupname" >> "$myoutfile"
+if (($isappend > 0));
+then
+    echo Appending to File: "$myoutfile"
+    echo ["$groupname"] >> "$myoutfile"
 else
-    echo "$groupname" > "$myoutfile"
+    echo ["$groupname"] > "$myoutfile"
 fi
 
 for i in ${servers//,/ }
 do
     echo "$i" >> "$myoutfile"
 done
-
